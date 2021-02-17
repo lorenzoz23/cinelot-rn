d16c106f5714e952ef38183c24137367540b59df
Lorenzo Zenitsky <lzep23@gmail.com>
2021-02-16 23:29:04 -0600

    removed search bar

--- screens/LotHome/LotHome.tsx
+++ screens/LotHome/LotHome.tsx
@@ -1,66 +1,33 @@
 import * as React from "react";
 import { SafeAreaView, ScrollView, StyleSheet, View } from "react-native";
-import { TextInput } from "react-native-gesture-handler";
 import MovieCard from "../../components/MovieCard";
-import { Feather } from "@expo/vector-icons";
 
 const LotHome = () => {
   return (
-    <View style={styles.container}>
-      <View
-        style={{
-          width: "100%",
-          paddingTop: 5,
-          paddingHorizontal: 5,
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "space-between",
-        }}
-      >
-        <TextInput
-          style={{
-            height: 40,
-            width: "75%",
-            borderColor: "gray",
-            borderWidth: 1,
-            borderRadius: 10,
-            paddingHorizontal: 10,
-            color: "white",
-          }}
-          editable
-          placeholder="Search your lot..."
-          placeholderTextColor="#BDC3C7"
-        />
-        <View style={{ paddingHorizontal: 10 }}>
-          <Feather name="filter" size={24} color="white" />
-        </View>
-      </View>
-      <SafeAreaView style={styles.container}>
-        <ScrollView style={{ width: "100%", paddingVertical: 10 }}>
-          {[...Array(20)].map(
-            (x, i) =>
-              i % 3 === 0 && (
-                <View
-                  style={{
-                    marginHorizontal: 10,
-                    marginBottom: 10,
-                    display: "flex",
-                    flexDirection: "row",
-                    width: "100%",
-                  }}
-                >
-                  {[...Array(4)].map((y, j) => (
-                    <View style={{ marginRight: 10 }}>
-                      <MovieCard key={j} />
-                    </View>
-                  ))}
-                </View>
-              )
-          )}
-        </ScrollView>
-      </SafeAreaView>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={{ width: "100%", paddingVertical: 10 }}>
+        {[...Array(20)].map(
+          (x, i) =>
+            i % 3 === 0 && (
+              <View
+                style={{
+                  marginHorizontal: 10,
+                  marginBottom: 10,
+                  display: "flex",
+                  flexDirection: "row",
+                  width: "100%",
+                }}
+              >
+                {[...Array(4)].map((y, j) => (
+                  <View style={{ marginRight: 10 }}>
+                    <MovieCard key={j} />
+                  </View>
+                ))}
+              </View>
+            )
+        )}
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
